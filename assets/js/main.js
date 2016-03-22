7f9652d231c932d21886ceb84ccc9aef88550a85
Jasmine Baik <boshnivolo@gmail.com>
2016-03-22 01:34:24 -0400

    Spring Cleaning

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".container").animate({left: 0});
+});
+
+var jazzPort = angular.module('JazzPortfolio', ['ngRoute'], function($routeProvider){
+    $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html'
+    })
+
+    .when('/aboutme', {
+      templateUrl: 'views/aboutme.html'
+    })
+
+    .when('/portfolio', {
+      templateUrl: 'views/portfolio.html'
+    })
+
+    .when('/resume', {
+      templateUrl: 'views/resume.html'
+    })
+
+    .when('/contact', {
+      templateUrl: 'views/contact.html'
+    })
+
+    .otherwise('/', {
+      templateUrl: 'views/home.html'
+    })
+
+  }) // END .when routing
